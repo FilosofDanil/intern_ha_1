49b70e468a9f9e1f46e5ec11fe4c8617cb7d58cc
FilosofDanil <serhio3347@gmail.com>
2024-04-07 17:58:36 +0200

    Added file reader and it's implementation.

---
+++ src/main/java/filereader/IFileReader.java
@@ -0,0 +1,5 @@
+package filereader;
+
+public interface IFileReader {
+    String read(String fileName);
+}
--- src/main/java/filereader/impl/FileReaderImpl.java
+++ src/main/java/filereader/impl/FileReaderImpl.java
@@ -1,20 +1,17 @@
-package filereader;
+package filereader.impl;
+
+import filereader.IFileReader;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-/*
-    Reader class, which is on DAO layer(we're accessing data from JSON)
- */
-public class FileReader {
-    //Make private constructor, in order to ensure no creation access from other classes, just to utilize class
-    private FileReader(){}
-    //temporary this method is static, however it's implementation will be changed
-    public static String read(String fileName){
+public class FileReaderImpl implements IFileReader {
+    @Override
+    public String read(String fileName){
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader br = java.nio.file.Files.newBufferedReader(Paths.get(fileName))) {
-           String line = br.readLine();
+            String line = br.readLine();
             while (line != null) {
                 stringBuilder.append(line);
                 line = br.readLine();
