cb0b48e450ef854ddb073ef0ebbe34f6aca49fc7
FilosofDanil <serhio3347@gmail.com>
2024-04-10 10:27:53 +0200

    Added gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import consoleInterface.ConsoleInterface;
+import services.counters.StatisticCounterContext;
+
+/**
+Entry point class Main
+ */
 public class Main {
     public static void main(String[]args){
-
+        StatisticCounterContext context = StatisticCounterContext.getInstance();
+        ConsoleInterface consoleInterface = new ConsoleInterface(context);
+        consoleInterface.start();
     }
 }
