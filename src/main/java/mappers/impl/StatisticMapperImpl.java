4dc66769c444dd4d6f4293ed7ecefd9d1fd96bc9
FilosofDanil <serhio3347@gmail.com>
2024-04-15 16:24:40 +0200

    Added StatisticMapper

---
+++ src/main/java/mappers/impl/StatisticMapperImpl.java
@@ -0,0 +1,34 @@
+package mappers.impl;
+
+import entities.Statistic;
+import mappers.StatisticMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class StatisticMapperImpl implements StatisticMapper {
+
+    private static StatisticMapperImpl statisticMapper;
+
+    private StatisticMapperImpl() {
+    }
+
+    @Override
+    public List<Statistic> mapToStatistic(Map<String, Integer> statisticMap) {
+        List<Statistic> statistics = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : statisticMap.entrySet()) {
+            Statistic statistic = new Statistic(entry.getKey(), entry.getValue());
+            statistics.add(statistic);
+        }
+        return statistics;
+    }
+
+    public static StatisticMapperImpl getInstance() {
+        if (statisticMapper == null) {
+            statisticMapper = new StatisticMapperImpl();
+        }
+        return statisticMapper;
+    }
+
+}
