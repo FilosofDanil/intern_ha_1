38b90fa6e4cf31369b90af32a912cdf2ee90aab5
FilosofDanil <serhio3347@gmail.com>
2024-04-17 22:04:04 +0200

    Deleted ConsoleInterface class. Deleted ExecutorService and it's implementation.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,20 @@
-import consoleInterface.ConsoleInterface;
-import filereader.IFileReader;
-import filereader.impl.FileReaderImpl;
-import services.counters.StatisticCounterContext;
+import entities.DefaultSettings;
+import lombok.extern.log4j.Log4j;
+import services.launcher.Launcher;
+import services.launcher.impl.LauncherImpl;
 
 /**
 Entry point class Main
  */
+@Log4j
 public class Main {
     public static void main(String[]args){
-        StatisticCounterContext context = StatisticCounterContext.getInstance();
-        ConsoleInterface consoleInterface = new ConsoleInterface(context);
-        consoleInterface.start();
+        Launcher launcher = LauncherImpl.getInstance();
+        if(args.length > 0){
+            launcher.launchReading(args[0], args[1]);
+        } else {
+            log.info("Program has started without parameters. Launching with default settings...");
+            launcher.launchReading(DefaultSettings.path, DefaultSettings.parameter);
+        }
     }
 }
